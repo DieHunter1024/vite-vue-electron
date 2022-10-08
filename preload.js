adaaa003cbe15eafb1240e296b04e474f4a354e8
zhangboyu <zhangboyu@hosecloud.com>
2022-10-08 18:03:37 +0800

    fix：name

--- preload.js
+++ preload.js
@@ -7,4 +7,4 @@ contextBridge.exposeInMainWorld("versions", {
   electron: () => process.versions.electron,
   // 能暴露的不仅仅是函数，我们还可以暴露变量
 });
-contextBridge.exposeInMainWorld("ipcRenderer", ipcRenderer); //app模块
+contextBridge.exposeInMainWorld("ipcRenderer", ipcRenderer); //ipcRenderer模块
