6a81c2298d3f2dd63d2303408ac80c3c29f52c2a
zhangboyu <zhangboyu@hosecloud.com>
2022-10-12 14:01:41 +0800

    feat：区分环境

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // 控制应用生命周期和创建原生浏览器窗口的模组
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
-
-// require('electron-reload')(path.join(__dirname, ""), {
-//   electron: require(`${__dirname}/node_modules/electron`)
+const { NODE_ENV } = process.env;
+// require("electron-reload")(path.join(__dirname, ""), {
+//   electron: require(`${__dirname}/node_modules/electron`),
 // });
 function createWindow() {
   // 创建浏览器窗口
@@ -14,15 +14,16 @@ function createWindow() {
       preload: path.join(__dirname, "preload.js"),
     },
   });
-
-  mainWindow.loadFile("./packages/app/dist/index.html");
-  // mainWindow.loadURL(`${"http://127.0.0.1"}:${2048}/`);
-  mainWindow.webContents.openDevTools();
+  if (NODE_ENV !== "production") {
+    mainWindow.loadURL(`${"http://127.0.0.1"}:${2048}/`);
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile("./packages/app/dist/index.html");
+  }
 }
 
 app.whenReady().then(() => {
   createWindow();
-
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
